25dc8252f7d53e10b268bef2b5b3ac20053a8946
444805970@qq.com <sha885260>
2022-04-12 20:58:04 +0800

    12345

---
+++ cloudalibaba-provider/src/main/java/com/sw/CloudalibabaProviderApplication.java
@@ -0,0 +1,13 @@
+package com.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderApplication.class, args);
+    }
+
+}
